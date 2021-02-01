da12bb9262fbe4a07f13b4c0d6b1129cbb440de9
oubani <oubaniayoub@gmail.com>
2021-02-01 21:14:17 +0100

    build tips

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './app.css';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import Thinks from './components/thinks/Thinks';
+import Tips from './components/tips/Tips';
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
       <Thinks />
+      <Tips />
     </div>
   );
 }
--- src/components/tips/Tips.js
+++ src/components/tips/Tips.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import styles from './Tips.module.css';
 
 const Tips = () => {
-  return <div></div>;
+  const { tips, tipsInfo, middle, upper } = styles;
+  return (
+    <div className={tips}>
+      <div className={tipsInfo}>
+        <h1>Tips for Travling in winter season</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, eius
+          velit doloremque, eius aspernatur et provident, eos cupiditate dolor.
+        </p>
+        <p>
+          tenetur Earum beatae corporis ipsa nam omnis, quia pariatur odio eum
+          velit doloremque,
+        </p>
+
+        <a href='/'>read more &#8594; </a>
+      </div>
+      <div className={middle}>
+        <img src='images/trip2.jpg' alt='' srcset='' />{' '}
+      </div>
+      <div className={upper}>
+        <img src='images/trip4.png' alt='winter' srcset='' />{' '}
+      </div>
+    </div>
+  );
 };
 
 export default Tips;
